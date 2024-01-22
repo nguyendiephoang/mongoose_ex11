5f593a8240ffd2a786ddd33436788e5714a0979c
Hoang <nguyendiephoang111@gmail.com>
2024-01-22 09:03:38 +0700

    Mongoose Part 2

--- index.js
+++ index.js
@@ -9,27 +9,49 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
-    });
+    // var newDish = Dishes({
+    //     name: 'Uthappizza13',
+    //     description: 'test'
+    // });
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+    // newDish.save()
+    //     .then((dish) => {
+    //         console.log(dish);
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
+    //         return Dishes.find({});
+    //     })
+    //     .then((dishes) => {
+    //         console.log(dishes);
 
-            // return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    //         return Dishes.remove({});
+    //     })
+    //     .then(() => {
+    //         return mongoose.connection.close();
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
+    Dishes.create({
+        	        name: 'Uthapizza',
+        	        description: 'Test'
+        	    })
+        	    .then((dish) => {
+        	        console.log(dish);
+        	        
+        	        return Dishes.find({}).exec();
+        	    })
+        	    .then((dishes) => {
+        	        console.log(dishes);
+        	
+        	        // return Dishes.remove({});
+        	    })
+        	    .then(() => {
+        	        return mongoose.connection.close();
+        	    })
+        	    .catch((err) => {
+        	        console.log(err);
+        	    });
+        
 
 });
