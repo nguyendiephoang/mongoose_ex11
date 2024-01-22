20276f94ed066461866ec00b2487e2d791dfe4ea
Hoang <nguyendiephoang111@gmail.com>
2024-01-22 21:44:04 +0700

    Mongoose Part 3

--- index.js
+++ index.js
@@ -9,49 +9,41 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    // var newDish = Dishes({
-    //     name: 'Uthappizza13',
-    //     description: 'test'
-    // });
-
-    // newDish.save()
-    //     .then((dish) => {
-    //         console.log(dish);
-
-    //         return Dishes.find({});
-    //     })
-    //     .then((dishes) => {
-    //         console.log(dishes);
-
-    //         return Dishes.remove({});
-    //     })
-    //     .then(() => {
-    //         return mongoose.connection.close();
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     });
-
     Dishes.create({
-        	        name: 'Uthapizza',
-        	        description: 'Test'
-        	    })
-        	    .then((dish) => {
-        	        console.log(dish);
-        	        
-        	        return Dishes.find({}).exec();
-        	    })
-        	    .then((dishes) => {
-        	        console.log(dishes);
-        	
-        	        // return Dishes.remove({});
-        	    })
-        	    .then(() => {
-        	        return mongoose.connection.close();
-        	    })
-        	    .catch((err) => {
-        	        console.log(err);
-        	    });
-        
+        name: 'Uthappizza2',
+        description: 'test'
+    })
+    .then((dish) =>{
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: {description: 'Updated test'}
+        },{
+            new: true
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author:'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        // return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 });
